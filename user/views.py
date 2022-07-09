a1accfabc48b1f648bc1c2bb7bd713892999c8cf
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-09 15:09:39 +0900

    Update : add annotation about errors

--- user/views.py
+++ user/views.py
@@ -19,6 +19,9 @@ class UserView(APIView):
         if serialized_data.is_valid():
             serialized_data.save()
             return Response(serialized_data.data, status=status.HTTP_200_OK)
+        
+        # 아래와 같이 errors를 프론트에 보여주는 것은 좋지 않은 방식이다. 현재는 내가 못하기 때문에 어디가 문제인지 바로 알 수 있어 좋지만
+        # 나중에 개발이 끝나고 나서는 그냥 '회원가입 실패'와 같은 메세지를 프론트로 보내주는 것이 더 좋을 것이다!
         return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
     
     # 회원정보 수정
