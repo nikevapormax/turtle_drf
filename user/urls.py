d9aa73a3156fea8faa8e664e711ecd12fcf01283
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-09 14:40:49 +0900

    Feat : set url for user signup

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from . import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView
 )
 
 urlpatterns = [
+    path('', views.UserView.as_view()),
+    
     # simplejwt 에서 제공하는 기본 JWT 인증 / 로그인 대체
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
