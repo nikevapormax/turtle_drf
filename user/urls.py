ee47d25aeeafba427d37004cd2a1e2ae1dac77ce
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-09 23:25:36 +0900

    Feat : apply custom jwt view at login

--- user/urls.py
+++ user/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', views.UserView.as_view()),
     
     # simplejwt 에서 제공하는 기본 JWT 인증 / 로그인 대체
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', views.TurtleTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
