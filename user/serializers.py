7a2f61fa6c2e7b9b80288cb53a8ca18d1c049966
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-09 15:08:35 +0900

    Feat : define password as an write_only field

--- user/serializers.py
+++ user/serializers.py
@@ -6,6 +6,11 @@ class UserSignupSerializer(serializers.ModelSerializer):
         model = UserModel
         fields  = ['username','password','email','fullname','join_date',]
         
+        extra_kwargs = {
+            # 쓰기 전용(write_only)으로 설정된 필드는 직렬화된 데이터에서 보여지지 않는다.
+            'password': {'write_only': True},
+        }
+        
     def create(self, *args, **kwargs):
         user = super().create(*args, **kwargs) # 상속받은 값
         
