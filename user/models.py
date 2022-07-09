8f0d950fbb6440b9481302ce7d67380089425e4a
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-09 09:17:56 +0900

    Feat : create user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError('Users must have an username.')
+        user = self.model(
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    def create_superuser(self, username, password=None):
+        user = self.create_user(
+            username=username,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    username = models.CharField("사용자 계정", max_length=30, unique=True)
+    password = models.CharField("비밀번호", max_length=128)
+    email = models.EmailField("이메일", max_length=100, unique=True)
+    fullname = models.CharField("사용자 이름", max_length=20)
+    join_date = models.DateTimeField("가입일자", auto_now_add=True)
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
+    
+    def __str__(self):
+        return f'{self.username} / {self.email} / {self.fullname}'
+
+    def has_perm(self, perm, obj=None):
+        return True
+    
+    def has_module_perms(self, app_label): 
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+    class Meta:
+        db_tale = 'user'
+        
+    
