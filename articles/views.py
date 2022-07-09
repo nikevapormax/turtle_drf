c499837fbb39a562ff95dded45c5da45c6c8d5d8
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-10 01:57:05 +0900

    Update : request to request.data

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Article as ArticleModel
+from .serializers import ArticleSerializer
+
+
+class ArticleView(APIView):
+    
+    def get(self, request):
+        articles = ArticleModel.objects.all()
+        serialized_data = ArticleSerializer(articles, many=True)
+        
+        return Response(serialized_data.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        user = request.user
+        request.data["user"] = user.id
+        
+        serialized_data = ArticleSerializer(data=request.data)
+        
+        if serialized_data.is_valid():
+            serialized_data.save()
+            return Response(serialized_data.data, status=status.HTTP_200_OK)
+        
+        return Response(serialized_data.errors, status=status.HTTP_400_BAD_REQUEST)
