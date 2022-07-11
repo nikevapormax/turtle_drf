b2b9533eec9ec066265315d439612ab921168258
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-12 00:16:01 +0900

    Feat : add url's name for test code

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,8 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
-    path('', views.UserView.as_view()),
+    # path에 name을 설정해주면, 나중에 url이 변경되더라도 테스트 코드는 변경하지 않아도 됨.
+    path('', views.UserView.as_view(), name="user_view"),
     
     # simplejwt 에서 제공하는 기본 JWT 인증 / 로그인 대체
     path('api/token/', views.TurtleTokenObtainPairView.as_view(), name='token_obtain_pair'),
