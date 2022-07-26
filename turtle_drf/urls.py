75572d58fb3f599663e1f90736cf588b284f7f78
Daegeun Jeong <wjdeorms27@naver.com>
2022-07-26 20:52:38 +0900

    Feat : add static url

--- turtle_drf/urls.py
+++ turtle_drf/urls.py
@@ -24,4 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('articles/', include('articles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
